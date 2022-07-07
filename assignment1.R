603f845e9dfe2397383aa48063c99a5ebba1cffb
Yunqi Gao <yunqigao@Yunqis-MacBook-Air.local>
2022-07-07 18:11:12 -0400

    completed

---
+++ assignment1.R
@@ -0,0 +1,41 @@
+
+# Create a variable 'x' that holds a value of 9
+x<- 9
+
+# Create a variable 'y' that holds a value of x
+y<- x
+
+# Create a variable 'z' that holds the sum of x and y
+z<- x+y
+
+# Clear the variable x from the workspace
+rm(x)
+
+# Clear all variables from the workspace
+rm(list=ls())
+
+# Make a statement that directs to http://www.google.com
+browseURL("http://www.google.com")
+
+# Show a list of all packages installed
+library()
+
+# Show a list of packages currently installed and usable
+search()
+
+# Magic Number Exercise
+# Do the Following:
+    # Choose any number and add 2 to it.
+x<- 5
+x+2
+y<- x+2
+    # Multiply the result by 3.
+7*3
+z<- y*3
+    # Subtract 6 from the answer.
+21-6
+f<- z-6
+    # Divide what you get by 3.
+15/3
+x<- f/3
+# you should end up with the same number that you started with
